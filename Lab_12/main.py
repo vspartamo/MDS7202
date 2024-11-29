6bdfb7020637a797fed9378662b704e838375642
Lucas-CE <lucas.carrascoestay@gmail.com>
2024-11-29 12:40:42 -0300

    Enhance main.py: optimize model loading, update model path, and add detailed docstrings for endpoints

--- Lab_12/main.py
+++ Lab_12/main.py
@@ -2,9 +2,13 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 import pickle
 import numpy as np
+import uvicorn
+from optimize import optimize_model
 
 
-model_path = "./Lab_12/models/best_model.pkl"
+optimize_model()
+
+model_path = "./docs/models/best_model.pkl"
 app = FastAPI()
 
 
@@ -23,6 +27,15 @@ class WaterSample(BaseModel):
 # Get
 @app.get("/")
 def home():
+    """
+    Home page
+
+    Parameters:
+    None
+
+    Returns:
+    dict: Information about the model
+    """
     return {
         "description": "Este modelo predice la potabilidad del agua con base en parámetros físico-químicos.",
         "problem": "Determinar si el agua es potable o no.",
@@ -44,6 +57,15 @@ def home():
 # Post
 @app.post("/potabilidad/")
 def predict_potabilidad(sample: WaterSample):
+    """
+    Predicts the potability of water
+
+    Parameters:
+    sample (WaterSample): Water sample to predict
+
+    Returns:
+    dict: Prediction of potability
+    """
     with open(model_path, "rb") as file:
         best_model = pickle.load(file)
     features = np.array(
@@ -64,3 +86,10 @@ def predict_potabilidad(sample: WaterSample):
     prediction = best_model.predict(features)[0]
 
     return {"potabilidad": int(prediction)}
+
+
+if __name__ == "__main__":
+    try:
+        uvicorn.run(app, host="0.0.0.0", port=7888)
+    except KeyboardInterrupt:
+        print("Server stopped. Goodbye! :)")
